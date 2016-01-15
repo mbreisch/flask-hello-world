637dcc1fe4c7ca29bc82f45019de1f4babfd8821
Matthew B. Reisch <m.reisch@amalgamatedanalytics.com>
2016-01-15 18:24:18 -0500

    First commit

---
+++ app.py
@@ -0,0 +1,15 @@
+# ---- Flask Hello World ---- #
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/hello")
+def hello_world():
+    return "Hello, world"
+
+
+if __name__ == "__main__":
+    app.run()
